58362ee2996b1e95d16d0edbbac6b4fea8805f1c
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-10-11 13:32:12 -0600

    Remove dependency of icons in pug file

--- android/app/src/main/kotlin/com/ajedora/tallytracker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ajedora.tallytracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/orfloresti/tallytracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.orfloresti.tallytracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
